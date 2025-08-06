f5c772d58e28b9c18dcd713950f052ac8e314047
cow64happy <122846453+cow64happy@users.noreply.github.com>
2025-08-06 11:59:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ st.set_page_config(page_title="재활용 지도 앱", page_icon="🌍", layout="
 st.title("♻️ 2023년 분리배출량 지도 보기")
 
 st.markdown(
-    '[🗺️ 2023년 지도 보기](https://cow64happy.github.io/recycle-map-app-2/recycle_map_2023.html
-)',
+    '[🗺️ 2023년 지도 보기](https://cow64happy.github.io/recycle-map-app-2/recycle_map_2023.html)',
     unsafe_allow_html=True
 )
